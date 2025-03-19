92aa7c8c0b89f605a74b485b340323cf6259a0f9
NnelgG <gescrimadora@gmail.com>
2025-03-19 16:26:37 +0800

    Initial commit - WP Dating Extension plugin

---
+++ wp-dating-extension.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: WP Dating Extension
+ * Description: A simple plugin to test Git workflow.
+ * Version: 1.0
+ * Author: Glenn Escrimadora
+ */
+
+if (!defined('ABSPATH')) {
+  exit; // Exit if accessed directly.
+}
+
+// Add the plugin settings menu
+function wp_dating_extension_add_menu() {
+  add_menu_page('WP Dating Extension', 'WP Dating Extension', 'manage_options', 'wp-dating-extension', 'wp_dating_extension_settings_page');
+}
+add_action('admin_menu', 'wp_dating_extension_add_menu');
+
+// Create settings page
+function wp_dating_extension_settings_page() {
+  echo '<div class="wrap"><h1>Hello world 1</h1></div>';
+}
