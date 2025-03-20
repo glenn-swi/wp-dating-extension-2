89349be3652d8174de7c333ab3d6cb30e66b2142
NnelgG <gescrimadora@gmail.com>
2025-03-20 14:19:47 +0800

    Just an update 5

--- wp-dating-extension.php
+++ wp-dating-extension.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Dating Extension 2
  * Description: A simple plugin to test Git workflow.
- * Version: 1.2
+ * Version: 1.3
  * Author: Glenn Escrimadora
  * GitHub Plugin URI: https://github.com/glenn-swi/wp-dating-extension-2/
  * Primary Branch: main 
@@ -19,6 +19,6 @@ function wp_dating_extension_add_menu_2() {
 add_action('admin_menu', 'wp_dating_extension_add_menu_2');
 
 // Create settings page
-function wp_dating_extension_settings_page_2() {
+function wp_dating_extension_settings_page_2 () {
   echo '<div class="wrap"><h1>Hello world 1</h1></div>';
 }
